6a4e96e1ce6217197c8bb4e44edce58219ad8a01
julia <julia8024@naver.com>
2022-03-19 15:24:53 +0900

    test_commit_4

--- app/src/main/java/com/example/homepage_notice_app/MainActivity.java
+++ app/src/main/java/com/example/homepage_notice_app/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
              public void onClick(View view) {
                  Toast.makeText(MainActivity.this, "Hello world", Toast.LENGTH_SHORT).show();
                  Toast.makeText(MainActivity.this, "Hello 3333", Toast.LENGTH_SHORT).show();
-                 Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
              }
          });
 
