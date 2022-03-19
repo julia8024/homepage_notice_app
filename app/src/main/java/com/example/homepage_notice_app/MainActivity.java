4b7ca788258fa621ae0210bd8e9e79047de49f6b
julia <julia8024@naver.com>
2022-03-19 16:51:01 +0900

    commit_frontend_1

--- app/src/main/java/com/example/homepage_notice_app/MainActivity.java
+++ app/src/main/java/com/example/homepage_notice_app/MainActivity.java
@@ -13,26 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         findViewById(R.id.test_button).setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 Toast.makeText(MainActivity.this, "Hello world", Toast.LENGTH_SHORT).show();
-                 Toast.makeText(MainActivity.this, "Hello 3333", Toast.LENGTH_SHORT).show();
-                 Toast.makeText(MainActivity.this, "Hello test_없어지진않겠지이이이이이", Toast.LENGTH_SHORT).show();
-
-                 // 안녕하세요
-                 int value = 0;
-                 for (int i = 0; i < 10; i++) {
-                     value++;
-                 }
-
-
-
-
-
-                int a = 0;
-             }
-         });
 
         // ㅎㅇㅎㅇ
     }
