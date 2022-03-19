0a5911ee1ca6e4c4a9e5ca80c2b02487e70a7757
julia <julia8024@naver.com>
2022-03-19 14:54:17 +0900

    test_commit

--- app/src/main/java/com/example/homepage_notice_app/MainActivity.java
+++ app/src/main/java/com/example/homepage_notice_app/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.homepage_notice_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         findViewById(R.id.test_button).setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+             }
+         });
+
+
     }
 }
